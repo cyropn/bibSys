1337595e6a6d300c784ac7b7557e253a9418df53
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2017-06-15 20:00:32 -0300

    Main view: Add MainJFrame
    This starts a main view implementation and the package structure.

---
+++ src/br/unesp/rc/BibSys/Main/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unesp.rc.BibSys.Main;
+
+import br.unesp.rc.BibSys.view.MainJFrame;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author filip
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame main = new MainJFrame();
+        main.setVisible(true);
+    }
+    
+}
