b593760200ac174cddc5156cc9d79dc7a108a8ed
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2017-06-16 15:21:46 -0300

    Main view: main JFrame was set to fullscreen.

--- src/br/unesp/rc/BibSys/Main/Main.java
+++ src/br/unesp/rc/BibSys/Main/Main.java
@@ -19,6 +19,7 @@ public class Main {
      */
     public static void main(String[] args) {
         JFrame main = new MainJFrame();
+        main.setExtendedState(JFrame.MAXIMIZED_BOTH); 
         main.setVisible(true);
     }
     
