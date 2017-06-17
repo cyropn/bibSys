efb6c3b2d230abf8304bc1ac08d0cf5093b7e4fd
Cyro Nascimento <cyropn@gmail.com>
2017-06-17 13:52:28 -0300

    Update javadoc

--- src/br/unesp/rc/BibSys/dao/BibTexDAOImpl.java
+++ src/br/unesp/rc/BibSys/dao/BibTexDAOImpl.java
@@ -3,9 +3,11 @@ package br.unesp.rc.BibSys.dao;
 import java.awt.List;
 
 public class BibTexDAOImpl {
+    
     /**
      * Retorna uma lista de bibTex que podem ser manipuladas pelos métodos da 
      * classe bibTexManager.
+     * 
      * @param path  caminha absoluto do arquivo a ser lido
      * @return      lista de bibTex que podem ser manipuladas pelos métodos
      */
@@ -13,8 +15,9 @@ public class BibTexDAOImpl {
         return null;
     }
     
-    /**
-     * Retorna se o arquivo salvou a string a ser salva 
+   /**
+     * Retorna se obteve sucesso em salvar a string desejada no arquivo
+     * 
      * @param bibTex    string a ser salva em um arquivo .bib
      * @return          verdadeiro se salvou ou falso se ocorreu algum erro
      */
@@ -23,7 +26,10 @@ public class BibTexDAOImpl {
     }
     
     /**
-     * Retorna se o update no arquivo foi realizado com sucesso
+     * Retorna se obteve sucesso em realizar um update no arquivo, que 
+     * consiste em apagar todo o conteudo antigo e salvar a nova string que
+     * será seu novo conteudo.
+     * 
      * @param bibTex    nova string que deve ser inserida no arquivo 
      * @return          verdadeiro se o update foi realizado com sucesso, 
      *                  caso contrario retorna falso.
