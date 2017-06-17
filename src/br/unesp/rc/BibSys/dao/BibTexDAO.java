55f9219bb84e20e7c30e0b270dc350a8dcb252e3
Cyro Nascimento <cyropn@gmail.com>
2017-06-17 13:23:24 -0300

    BibTexDao: javadoc

--- src/br/unesp/rc/BibSys/dao/BibTexDAO.java
+++ src/br/unesp/rc/BibSys/dao/BibTexDAO.java
@@ -1,11 +1,27 @@
 package br.unesp.rc.BibSys.dao;
 
 public interface BibTexDAO {
-    
+    /**
+     * Retorna uma lista de bibTex que podem ser manipuladas pelos métodos da 
+     * classe bibTexManager.
+     * @param path  caminha absoluto do arquivo a ser lido
+     * @return      lista de bibTex que podem ser manipuladas pelos métodos
+     */
     public List<BibTex> read(String path);
     
+    /**
+     * Retorna se o arquivo salvou a string a ser salva 
+     * @param bibTex    string a ser salva em um arquivo .bib
+     * @return          verdadeiro se salvou ou falso se ocorreu algum erro
+     */
     public boolean create(String bibTex);
     
+    /**
+     * Retorna se o update no arquivo foi realizado com sucesso
+     * @param bibTex    nova string que deve ser inserida no arquivo 
+     * @return          verdadeiro se o update foi realizado com sucesso, 
+     *                  caso contrario retorna falso.
+     */
     public boolean update(String bibTex);
     
 }
