3e6d46daf96351cd0a59156f2f00852a1f8c41ce
Cyro Nascimento <cyropn@gmail.com>
2017-06-17 18:10:16 -0300

    Implementation of create

--- src/br/unesp/rc/BibSys/dao/BibTexDAOImpl.java
+++ src/br/unesp/rc/BibSys/dao/BibTexDAOImpl.java
@@ -1,8 +1,25 @@
 package br.unesp.rc.BibSys.dao;
 
+import br.unesp.rc.BibSys.beans.BibTex;
 import java.awt.List;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class BibTexDAOImpl {
+/**
+ * Esta classe é a implementação dos métodos do interface BibTexDAO. 
+ * Este métodos tem por objetivo a manipulação de arquivo, comoler, criar 
+ * e fazer uma atualização.
+ * 
+ * @author cyro
+ * @see BibTexDAOImpl
+ */
+public class BibTexDAOImpl implements BibTexDAO{
+    
+    public BibTexDAOImpl(){      
+    }
     
     /**
      * Retorna uma lista de bibTex que podem ser manipuladas pelos métodos da 
@@ -11,9 +28,9 @@ public class BibTexDAOImpl {
      * @param path  caminha absoluto do arquivo a ser lido
      * @return      lista de bibTex que podem ser manipuladas pelos métodos
      */
-    public List<BibTex> read(String path){
+   /* public List<> read(String path){
         return null;
-    }
+    }*/
     
    /**
      * Retorna se obteve sucesso em salvar a string desejada no arquivo
@@ -21,8 +38,18 @@ public class BibTexDAOImpl {
      * @param bibTex    string a ser salva em um arquivo .bib
      * @return          verdadeiro se salvou ou falso se ocorreu algum erro
      */
-    public boolean create(String bibTex){
-        return false;
+    public boolean create(String bibTex, File file) {
+        boolean b = false;
+        try {
+            file.createNewFile();
+            FileWriter fw = new FileWriter(file);
+            fw.write(bibTex);
+            fw.flush();
+            b = true;
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+        return b;
     }
     
     /**
